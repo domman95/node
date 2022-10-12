ae7d9bcf775f2066ec79330585325b2de3df4de2
domman95 <dominiklasek95@gmail.com>
2022-10-12 15:15:10 +0200

    changed port

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
     res.send("Welcom to the home page!");
 });
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Your server is running on: http://localhost:${PORT}`);
+    console.log(`🚀 Your server is running on: ${PORT}`);
 });
